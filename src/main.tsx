d6b48cb80db27c7a1ea4dac79f4edf912dcae048
Malak Sabry <malaksabry367@gmail.com>
2025-07-14 18:03:50 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ErrorPage from "./Routes/ErrorPage.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<ErrorPage />} />
+
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
